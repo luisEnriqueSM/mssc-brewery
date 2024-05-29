cc2dff7ad893d705e7c3475afd98b34d1c84c9b5
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-05-29 17:05:50 -0600

    Add PostMapping, PutMapping and DeleteMapping in CustomerController, created and implemented new CustomerServices methods in CustomerServiceImpl

--- src/main/java/org/beer/works/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/org/beer/works/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package org.beer.works.msscbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.beer.works.msscbrewery.web.model.CustomerDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService{
     @Override
@@ -13,5 +15,23 @@ public class CustomerServiceImpl implements CustomerService{
                 .id(UUID.randomUUID())
                 .name("Luis")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl - would add a real impl to update customer
+        log.debug("Updating a customer...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
     }
 }
