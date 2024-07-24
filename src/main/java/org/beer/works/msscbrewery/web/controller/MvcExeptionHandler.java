c0e45f12c27cf080914954770310b55d1c5546c8
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-24 16:01:27 -0600

    Replace ConstraintViolationException.class to MethodArgumentNotValidException.class for validationErrorHandler method in ControllerAdvice. Fix BeerController test. Closes #1

--- src/main/java/org/beer/works/msscbrewery/web/controller/MvcExeptionHandler.java
+++ src/main/java/org/beer/works/msscbrewery/web/controller/MvcExeptionHandler.java
@@ -1,31 +1,23 @@
 package org.beer.works.msscbrewery.web.controller;
 
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
-public class MvcExeptionHandler extends ResponseEntityExceptionHandler {
+public class MvcExeptionHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex){
-        List<String> errors = new ArrayList<>(ex.getConstraintViolations().size());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List> validationErrorHandler(MethodArgumentNotValidException ex){
+        List<String> errorsList = new ArrayList<>(ex.getErrorCount());
 
-        ex.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-        return  new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
+        ex.getFieldErrors().forEach(error -> errorsList.add(error.getObjectName() + "." + error.getField() + " : " + error.getDefaultMessage()));
 
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<List> handleBindException(BindException ex){
-        return new ResponseEntity<>(ex.getAllErrors(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
     }
 }
